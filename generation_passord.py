181a6e9c4eddcb26152e26f731fff7695d887c17
Vovisim <Wova53209@yandex.ru>
2023-10-18 14:37:05 +0300

    создание файла с генерацией паролей

--- generation_passord.py
+++ generation_passord.py
@@ -1,13 +1,42 @@
-from random import randint as rand
+from random import shuffle, choice, randint
 
-l = "F9l25LA$E"
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 alfabet= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-special_characters = "#, %, &, !, ?"
-
-for i in l:
-    print(ord(i))
-
+special_characters = ["#, %, &, !, ?"]
 
 def GenerationPass(passlen=8, dot="True", SpecialCharacters="False"):
+    passlen1 = passlen
+    if SpecialCharacters == False:
+        lenspecial = 0
+        if passlen % 2 != 0:
+            passlen1 -= 1
+    elif passlen % 2 == 0:
+        lenspecial = 2
+        passlen1 -= 2
+    else:
+        lenspecial = 1
+        passlen -= 1
+    prepass = []
+    for i in passlen1//2:
+        prepass.append(choice(numbers))
+    for i in passlen1//2:
+        prepass.append(choice(alfabet))
+    if dot == True:
+        prepass.append(".")
+        lenspecial -= 1
+
+    for i in lenspecial:
+        prepass.append(choice(special_characters))
+
+    if len(prepass) > passlen:
+        prepass.pop
+    return prepass
+
+print(GenerationPass(8, True, False))
+
+
+
+
+
+
 
